b4a0b98488d0f539090bc59dbf682cf5aab3c669
Jalaluddin SS <94308513+randomshots@users.noreply.github.com>
2025-05-14 17:32:13 +0530

    Added the first chapter's files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Header from './header.jsx'
+import Footer from './Footer.jsx'
+import Food from './Food.jsx'
+import Test1 from './Test1.jsx'
+
+function App() {
+
+  return(
+    <>
+      <Header/>
+      <Food/>
+      <Food/>
+      <Test1/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App
---
+++ src/Test1.jsx
@@ -0,0 +1,13 @@
+
+function Test1() {
+    return (
+        <div>
+            <h1>This is the test1 component</h1>
+            <p>I window</p>
+            <p>Are u rendering????</p>
+            
+        </div>
+    );
+}
+
+export default Test1
