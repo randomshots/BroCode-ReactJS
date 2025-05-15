cdc4efc9b93717d8111d322089a18180356783f4
Jalaluddin SS <94308513+randomshots@users.noreply.github.com>
2025-05-15 22:49:22 +0530

    3rd part of 3rd chapter's files
    inline css styling is easy and convenient to understand. It prevents global style conflicts because we are not working with class names. it's great for isolated components with minimal styling, such as a like button/subscribe button. However, inline styling can be less maintainable

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,8 +1,18 @@
 
 function Button() {
 
+    const styles = {
+        backgroundColor: "hsl(200, 100%, 50%)",
+        color: "white",
+        padding: "10px 20px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",
+    };
+
+
     return (
-        <button className = "button">Click Me</button>
+        <button style = {styles}>Click Me</button>
     );
 }
 
